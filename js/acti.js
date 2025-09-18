9564f7ec157c27328e6b06c2e8dbfbeb6c363c36
technoportail <timothee.capaldi@ac-lyon.fr>
2025-09-18 18:05:49 +0200

    Add files via upload
    firstpush

---
+++ js/acti.js
@@ -0,0 +1,21 @@
+    // ----------------------------
+    // Données JSON intégrées ici
+    // ----------------------------
+    const documents = [
+      {
+        "titre": "Activité 1 : Exprimer un Besoin",
+        "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSfaXVKoSO592ie9xqehmhJ8pxwoTTBqOO9YHjjbr5xhFScXbQ/viewform?usp=dialog",
+		"im": "img/vignettes_acti/acti1.jpg",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "5e",
+        "type": "acti_num"
+      },
+      {
+        "titre": "Activité 1 : Evolution des objets",
+        "fichier": "",
+		"im": "img/vignettes_acti/acti2.jpg",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "3e",
+        "type": "acti_num"
+      }
+    ];
---
+++ js/docs.js
@@ -0,0 +1,64 @@
+    // ----------------------------
+    // Données JSON intégrées ici
+    // ----------------------------
+    const documents = [
+      {
+        "titre": "Boîte Mystère",
+        "fichier": "1_5eme/chap1/0_boite_mystere_activite_de_declenchement.pdf",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "5e",
+        "type": "acti_groupe"
+      },
+      {
+        "titre": "Exprimer un besoin",
+        "fichier": "1_5eme/chap1/1_exprimer_un_besoin_cours.pdf",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "5e",
+        "type": "cours"
+      },
+	  {
+        "titre": "Timeline en classe",
+        "fichier": "3_3eme/chap1/0_Timeline_en_classe_activite_de_declenchement.pdf",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "3e",
+        "type": "acti_groupe"
+      },
+	  {
+        "titre": "L'influence de la technologie",
+        "fichier": "3_3eme/chap1/1_influence_de_la_technologie_cours.pdf",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "3e",
+        "type": "cours"
+      },
+	  {
+        "titre": "Presentation d'un objet",
+        "fichier": "3_3eme/chap1/2_presentation_evolution_d_un_objet_technique.pdf",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "3e",
+        "type": "pres"
+      },
+	  
+	  // Synthèses
+	  
+	  {
+        "titre": "Evolution des besoins",
+        "fichier": "4_syntheses/1_OST/OST1a.pdf",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      },
+	  {
+        "titre": "Fonction, principe technique",
+        "fichier": "4_syntheses/1_OST/OST1b.pdf",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      },
+	  {
+        "titre": "Famille et lignée des OST",
+        "fichier": "4_syntheses/1_OST/OST1c.pdf",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      }
+    ];
