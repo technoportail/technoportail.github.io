0f179f5d39efb0b79dfb39cc58db41a1184bb567
technoportail <timothee.capaldi@ac-lyon.fr>
2025-09-22 16:50:23 +0200

    Update docs.js
    added some syntheses

--- js/docs.js
+++ js/docs.js
@@ -37,29 +37,95 @@
         "classe": "3e",
         "type": "pres"
       },
+	  {
+        "titre": "Presentation d'un objet (ressources)",
+        "fichier": "",
+        "theme": "Découvrir et analyser les OST",
+        "classe": "3e",
+        "type": "cours"
+      },
 	  
-	  // Synthèses
-	  
+	  // Synthèses OST
+	  {
+        "titre": "Liste des fiches connaissances",
+        "fichier": "https://drive.google.com/file/d/15YYnd6guOjgGBfKFzU_VtpAJBNU_LbPi/view?usp=drive_link",
+        "theme": "Synthèses",
+        "classe": "liste",
+        "type": "synth"
+      },
 	  {
         "titre": "Evolution des besoins",
-        "fichier": "4_syntheses/1_OST/OST1a.pdf",
+        "fichier": "https://drive.google.com/file/d/1E--GPEFza7U3pmlBA7iQpbGbXRZlq0gS/view?usp=drive_link",
         "theme": "Synthèses",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Fonction, principe technique",
-        "fichier": "4_syntheses/1_OST/OST1b.pdf",
+        "fichier": "https://drive.google.com/file/d/1oD0dCo-Cv8xWzp55ArtlXNaCD1C19VER/view?usp=drive_link",
         "theme": "Synthèses",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Famille et lignée des OST",
-        "fichier": "4_syntheses/1_OST/OST1c.pdf",
+        "fichier": "https://drive.google.com/file/d/1tMhhdOK0RMpiqbW0yFdYYAvEKsDT5dz1/view?usp=drive_link",
         "theme": "Synthèses",
         "classe": "OST",
         "type": "synth"
+      },
+	  {
+        "titre": "Contraintes sociétales",
+        "fichier": "https://drive.google.com/file/d/1kjg0l7mYl6s8eWBm7L10qFSze5kzZQCV/view?usp=drive_link",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      },
+	  {
+        "titre": "Type d'apprentissage IA et usages",
+        "fichier": "https://drive.google.com/file/d/1Rt838BHoOV5WF9y1nJ2xZE4bTotfEFZX/view?usp=drive_link",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      },
+	  {
+        "titre": "Incidences sociétales de l'IA",
+        "fichier": "https://drive.google.com/file/d/1yYPivAud7X4vZwkltiHOz3ZYtAV1hj36/view?usp=drive_link",
+        "theme": "Synthèses",
+        "classe": "OST",
+        "type": "synth"
+      },
+	
+	  //Synthèses SFC
+	  
+	  {
+        "titre": "Réseau local / mondial",
+        "fichier": "",
+        "theme": "Synthèses",
+        "classe": "SFC",
+        "type": "synth"
+      },
+	  {
+        "titre": "Rôle des composants d'un réseau",
+        "fichier": "",
+        "theme": "Synthèses",
+        "classe": "SFC",
+        "type": "synth"
+      },
+	  {
+        "titre": "Adresse IP / Tables de routage",
+        "fichier": "",
+        "theme": "Synthèses",
+        "classe": "SFC",
+        "type": "synth"
+      },
+	  {
+        "titre": "Notion de débit / Ordre de grandeur",
+        "fichier": "",
+        "theme": "Synthèses",
+        "classe": "SFC",
+        "type": "synth"
       }
-
+	  
     ];
+
