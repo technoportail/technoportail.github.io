a41c4a2140a496f4e779d3ba68403e61a6c11227
technoportail <timothee.capaldi@ac-lyon.fr>
2025-11-03 20:58:15 +0100

    Refactor JSON data structure in docs.js

--- js/docs.js
+++ js/docs.js
@@ -76,85 +76,92 @@
 	  {
         "titre": "Liste des fiches connaissances",
         "fichier": "https://drive.google.com/file/d/15YYnd6guOjgGBfKFzU_VtpAJBNU_LbPi/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "liste",
         "type": "synth"
       },
 	  {
         "titre": "Evolution des besoins",
         "fichier": "https://drive.google.com/file/d/1E--GPEFza7U3pmlBA7iQpbGbXRZlq0gS/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Fonction, principe technique",
         "fichier": "https://drive.google.com/file/d/1oD0dCo-Cv8xWzp55ArtlXNaCD1C19VER/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Famille et lignée des OST",
         "fichier": "https://drive.google.com/file/d/1tMhhdOK0RMpiqbW0yFdYYAvEKsDT5dz1/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Contraintes sociétales",
         "fichier": "https://drive.google.com/file/d/1kjg0l7mYl6s8eWBm7L10qFSze5kzZQCV/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Type d'apprentissage IA et usages",
         "fichier": "https://drive.google.com/file/d/1Rt838BHoOV5WF9y1nJ2xZE4bTotfEFZX/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	  {
         "titre": "Incidences sociétales de l'IA",
         "fichier": "https://drive.google.com/file/d/1yYPivAud7X4vZwkltiHOz3ZYtAV1hj36/view?usp=drive_link",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "OST",
         "type": "synth"
       },
 	
 	  //Synthèses SFC
-	  
+	  {
+        "titre": "Chaîne d'énergie",
+        "fichier": "https://drive.google.com/file/d/1m8JeBqqSqmFlRyD_2MZvQEdDYnXa0gC_/view?usp=drive_link",
+        "theme": "Fiches connaissances",
+        "classe": "SFC",
+        "type": "synth"
+      },
 	  {
         "titre": "Réseau local / mondial",
         "fichier": "",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "SFC",
         "type": "synth"
       },
 	  {
         "titre": "Rôle des composants d'un réseau",
         "fichier": "",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissancess",
         "classe": "SFC",
         "type": "synth"
       },
 	  {
         "titre": "Adresse IP / Tables de routage",
         "fichier": "",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "SFC",
         "type": "synth"
       },
 	  {
         "titre": "Notion de débit / Ordre de grandeur",
         "fichier": "",
-        "theme": "Synthèses",
+        "theme": "Fiches connaissances",
         "classe": "SFC",
         "type": "synth"
       }
 	  
     ];
+
 
 
 
