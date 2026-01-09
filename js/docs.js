79565f363d5fdd9fd6811652459ab8740cb92909
technoportail <timothee.capaldi@ac-lyon.fr>
2026-01-09 13:20:37 +0100

    Add chapter details to documents JSON
    Added chapter information to existing documents.

--- js/docs.js
+++ js/docs.js
@@ -8,6 +8,7 @@
         "fichier": "https://drive.google.com/file/d/1hW1JpRpdBtQmmsE8N073wD8xwXGUvqHu/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "acti_groupe"
       },
       {
@@ -15,6 +16,7 @@
         "fichier": "https://drive.google.com/file/d/1tiBTOXwjE96TERRfUZTthT6C-nKBV-QP/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "cours"
       },
 		{
@@ -22,6 +24,7 @@
         "fichier": "https://drive.google.com/file/d/1rJxH25fdjdWuQhlGoRbXPui__n_9GkQw/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "acti_groupe"
       },
 		{
@@ -29,13 +32,15 @@
         "fichier": "https://drive.google.com/file/d/1j5SFiO9xKMlob4E2AcHD25ub_rhllbSS/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "synth"
       },
 		{
-        "titre": "Besoin/fonction technique (Exercices)",
+        "titre": "Besoin/fonction technique",
         "fichier": "https://drive.google.com/file/d/1yeZk6297KQ1f45FJVkZxtSA9nK49j2Y7/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "acti_ecrite"
       },
 	  {
@@ -43,6 +48,7 @@
         "fichier": "https://drive.google.com/file/d/11U9stlaM3z_iOFLss5w-fyu5I4141JX_/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "5e",
+		"chapitre": "Chapitre 1\nEnvironnement des OST",
         "type": "cours"
       },
 	  {
@@ -50,6 +56,7 @@
         "fichier": "https://drive.google.com/file/d/1LRd7fK51fAk4POMorhtXQPju5DEVtXF_/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "3e",
+		"chapitre": "Chapitre 1\nEvolution des OST",
         "type": "acti_groupe"
       },
 	  {
@@ -57,6 +64,7 @@
         "fichier": "https://drive.google.com/file/d/14tznFGWI7wjdm72JGXtdUD-su6s5UKmj/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "3e",
+		"chapitre": "Chapitre 1\nEvolution des OST",
         "type": "cours"
       },
 	  {
@@ -64,6 +72,7 @@
         "fichier": "https://drive.google.com/file/d/1IPCt5XYOZht6wCyujQEOeAsVHLdNFr4H/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "3e",
+		"chapitre": "Chapitre 1\nEvolution des OST",
         "type": "pres"
       },
 	  {
@@ -71,6 +80,7 @@
         "fichier": "https://drive.google.com/file/d/1F2q3_hTyuivu1c6SJN-ZtSujA7_FwH-h/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "3e",
+		"chapitre": "Chapitre 1\nEvolution des OST",
         "type": "cours"
       },
 	  {
@@ -78,6 +88,7 @@
         "fichier": "https://drive.google.com/file/d/1O2cz5sCdt6NpJkId0xisx_e2B6x92EpM/view?usp=drive_link",
         "theme": "Découvrir et analyser les OST",
         "classe": "3e",
+		"chapitre": "Chapitre 1\nEvolution des OST",
         "type": "synth"
       },
 	  // Structure et fonctionnement des OST
@@ -86,6 +97,7 @@
         "fichier": "https://drive.google.com/file/d/1uM_3P_nuu0w1CJMdha8miZvT0PeT8DrO/view?usp=drive_link",
         "theme": "Structure et fonctionnement des OST",
         "classe": "5e",
+		"chapitre": "Chapitre 2\nStructure des ponts",
         "type": "acti_groupe"
       },
 	  {
@@ -93,6 +105,7 @@
         "fichier": "https://drive.google.com/file/d/1t7t9JTFuNGUbTej0eWXFMA7IN7of2v0u/view?usp=drive_link",
         "theme": "Structure et fonctionnement des OST",
         "classe": "5e",
+		"chapitre": "Chapitre 2\nStructure des ponts",
         "type": "pres"
       },
 	  {
@@ -100,14 +113,48 @@
         "fichier": "https://drive.google.com/file/d/1drzD8NIvnhU9rHbV3Q-bOF5uj6K8w-Do/view?usp=drive_link",
         "theme": "Structure et fonctionnement des OST",
         "classe": "5e",
+		"chapitre": "Chapitre 2\nStructure des ponts",
         "type": "cours"
+      },
+	  {
+        "titre": "Reconnaître le vrai du faux",
+        "fichier": "https://drive.google.com/file/d/11IFNx9AJDzc-9tZq1DWbVX-qPSNxQf3F/view?usp=drive_link",
+        "theme": "Structure et fonctionnement des OST",
+        "classe": "5e",
+		"chapitre": "Chapitre 3\nCybersécurité et IA",
+        "type": "acti_groupe"
+      },
+	  {
+        "titre": "Les Composants d'Internet",
+        "fichier": "",
+        "theme": "Structure et fonctionnement des OST",
+        "classe": "3e",
+		"chapitre": "Chapitre 2\nRéseaux Informatiques",
+        "type": "cours_manu"
       },
 	  {
         "titre": "Réseau internet et www",
         "fichier": "https://drive.google.com/file/d/1SEJaC2jcjsi26b95_HvsVgHFff6zVsg9/view?usp=drive_link",
         "theme": "Structure et fonctionnement des OST",
         "classe": "3e",
+		"chapitre": "Chapitre 2\nRéseaux Informatiques",
         "type": "cours"
+      },
+	  {
+        "titre": "L'Adresse IP",
+        "fichier": "",
+        "theme": "Structure et fonctionnement des OST",
+        "classe": "3e",
+		"chapitre": "Chapitre 2\nRéseaux Informatiques",
+        "type": "cours_manu"
+      },
+	  {
+        "titre": "Programmation Microbit",
+        "fichier": "https://drive.google.com/file/d/13h0DVnZfeY_yIzGLk3VlvN3IH8T8xSua/view?usp=drive_link",
+        "theme": "Structure et fonctionnement des OST",
+        "classe": "3e",
+		"chapitre": "Chapitre 3\nInformatique Embarquée",
+        "type": "acti_num"
       },
 		
 	  // Synthèses OST
@@ -116,6 +163,7 @@
         "fichier": "https://drive.google.com/file/d/15YYnd6guOjgGBfKFzU_VtpAJBNU_LbPi/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "liste",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -123,6 +171,7 @@
         "fichier": "https://drive.google.com/file/d/1E--GPEFza7U3pmlBA7iQpbGbXRZlq0gS/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -130,6 +179,7 @@
         "fichier": "https://drive.google.com/file/d/1oD0dCo-Cv8xWzp55ArtlXNaCD1C19VER/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -137,6 +187,7 @@
         "fichier": "https://drive.google.com/file/d/1tMhhdOK0RMpiqbW0yFdYYAvEKsDT5dz1/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -144,6 +195,7 @@
         "fichier": "https://drive.google.com/file/d/1kjg0l7mYl6s8eWBm7L10qFSze5kzZQCV/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -151,6 +203,7 @@
         "fichier": "https://drive.google.com/file/d/1Rt838BHoOV5WF9y1nJ2xZE4bTotfEFZX/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -158,6 +211,7 @@
         "fichier": "https://drive.google.com/file/d/1yYPivAud7X4vZwkltiHOz3ZYtAV1hj36/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "OST",
+		"chapitre": "Synthèses",
         "type": "synth"
       },		
 	  //Synthèses SFC
@@ -166,6 +220,7 @@
         "fichier": "https://drive.google.com/file/d/1m8JeBqqSqmFlRyD_2MZvQEdDYnXa0gC_/view?usp=drive_link",
         "theme": "Fiches connaissances",
         "classe": "SFC",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -173,6 +228,7 @@
         "fichier": "",
         "theme": "Fiches connaissances",
         "classe": "SFC",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -180,6 +236,7 @@
         "fichier": "",
         "theme": "Fiches connaissances",
         "classe": "SFC",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -187,6 +244,7 @@
         "fichier": "",
         "theme": "Fiches connaissances",
         "classe": "SFC",
+		"chapitre": "Synthèses",
         "type": "synth"
       },
 	  {
@@ -194,19 +252,11 @@
         "fichier": "",
         "theme": "Fiches connaissances",
         "classe": "SFC",
+		"chapitre": "Synthèses",
         "type": "synth"
       }
 	  
     ];
-
-
-
-
-
-
-
-
-
 
 
 
