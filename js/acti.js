9c0bb088a5d442a35deaabbd3db994541845243a
technoportail <timothee.capaldi@ac-lyon.fr>
2026-01-09 13:22:56 +0100

    Revise document themes and classes in JSON data
    Updated themes and classes for several documents in the JSON data.

--- js/acti.js
+++ js/acti.js
@@ -1,4 +1,4 @@
-    // ----------------------------
+	// ----------------------------
     // Données JSON intégrées ici
     // ----------------------------
     const documents = [
@@ -6,7 +6,7 @@
         "titre": "Exprimer un Besoin",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSfaXVKoSO592ie9xqehmhJ8pxwoTTBqOO9YHjjbr5xhFScXbQ/viewform?usp=dialog",
 		"im": "img/vignettes_acti/acti_besoin.jpg",
-        "theme": "Découvrir et analyser les OST",
+        "theme": "Cahier des charges",
         "classe": "5e",
         "type": "acti_num"
       },
@@ -14,7 +14,7 @@
         "titre": "Trotti-shop, le magasin de trottinettes",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSdQiQxHAbu0s38rn9ok_MMBdpyS0TkCSmfgInGBx_Ruk5dimQ/viewform?usp=dialog",
 		"im": "img/vignettes_acti/trottishop.jpg",
-        "theme": "Découvrir et analyser les OST",
+        "theme": "Cahier des charges",
         "classe": "5e",
         "type": "acti_num"
       },
@@ -22,23 +22,23 @@
         "titre": "Formes d'énergies dans les objets du quotidien",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLScG1546isQcf2Jw4cWD_EcIt_ydXsgFu5d-OfH-NP2Bxpo2Jw/viewform?usp=dialog",
 		"im": "img/vignettes_acti/forme_ener.jpg",
-        "theme": "Découvrir et analyser les OST",
+        "theme": "Chaîne d'énergie",
         "classe": "5e",
-        "type": "acti_num"
-      },
-      {
-        "titre": "Evolution des objets",
-        "fichier": "",
-		"im": "img/vignettes_acti/evolution_obj.jpg",
-        "theme": "Découvrir et analyser les OST",
-        "classe": "3e",
         "type": "acti_num"
       },
 	  {
         "titre": "Chaine d'énergie des objets du quotidien",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLScW2giDu6jSCWzIlFe1GfRFMCcTaxyyUxYaBkYrvCerL4usuA/viewform?usp=dialog",
 		"im": "img/vignettes_acti/chaine_ener_moteur.jpg",
-        "theme": "Structure et fonctionnement des OST",
+        "theme": "Chaîne d'énergie",
+        "classe": "4e",
+        "type": "acti_num"
+      },
+	  {
+        "titre": "Choisir un pont adapté à une situation ",
+        "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSfyRYqEiIR8Fii40qFJ4W2Hpx_vNFefj2wj2AJG_2c90JyaqQ/viewform?usp=dialog",
+		"im": "img/vignettes_acti/pont_situation.jpg",
+        "theme": "Structure des ponts",
         "classe": "5e",
         "type": "acti_num"
       },
@@ -46,15 +46,7 @@
         "titre": "Internet, la pollution cachée",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSfuepTOXMgCGpXCU97Y7pUaGZHUV7OBQuuTMXU6esxnzf7efw/viewform?usp=dialog",
 		"im": "img/vignettes_acti/internet_impact.jpg",
-        "theme": "Structure et fonctionnement des OST",
-        "classe": "3e",
-        "type": "acti_num"
-      },
-	  {
-        "titre": "Internet, simuler un réseau",
-        "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSd1DH7bLXjyf5AznTP3wcOx-NTwd68ye5P9bdlmdwItfuXJWQ/viewform?usp=dialog",
-		"im": "img/vignettes_acti/internet_filius.jpg",
-        "theme": "Structure et fonctionnement des OST",
+        "theme": "Réseaux Informatiques",
         "classe": "3e",
         "type": "acti_num"
       },
@@ -62,13 +54,16 @@
         "titre": "Composants d'un réseau et IP",
         "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSeXUE97PRR_QputAFGyOBSs1suIRxVPKeNmomp0t0_Wif2REA/viewform?usp=dialog",
 		"im": "img/vignettes_acti/internet_tcp.jpg",
-        "theme": "Structure et fonctionnement des OST",
+        "theme": "Réseaux Informatiques",
+        "classe": "3e",
+        "type": "acti_num"
+      },
+	  {
+        "titre": "Internet, simuler un réseau",
+        "fichier": "https://docs.google.com/forms/d/e/1FAIpQLSd1DH7bLXjyf5AznTP3wcOx-NTwd68ye5P9bdlmdwItfuXJWQ/viewform?usp=dialog",
+		"im": "img/vignettes_acti/internet_filius.jpg",
+        "theme": "Réseaux Informatiques",
         "classe": "3e",
         "type": "acti_num"
       }
 ];
-
-
-
-
-
